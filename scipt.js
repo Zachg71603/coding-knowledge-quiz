b70afc319978ed130fce643c90c0a806f377183c
Zach Gutierrez <zg71603@gmail.com>
2024-01-02 23:58:31 -0800

    Starting JS

---
+++ scipt.js
@@ -0,0 +1,34 @@
+var questionArray = [
+    
+    {
+        question: "What are the 3 main programming languages of the internet?",
+        answers: ["jQuery, MongoDB, Python","HTML, CSS, JavaScript","C++, CSharp, Unity"],
+        correctAnswer: "HTML, CSS, JavaScript",    
+  
+    },
+    {
+        question: "What type of event in JavaScript makes a button work?",
+        answers: ["clickOn","doubleClick","onClick",],
+        correctAnswer: "onClick"
+           
+    },
+    {
+        question: "What is one way you can make a website mobile-friendly?",
+        answers: ["Media Queries","Smaller font sizes", "You dont, websites are automatically mobile-friendly"],
+        correctAnswer: "Media Queries"  
+         
+    },
+    {
+        question: "In web development, which of the following is used to call HTML ID elements from a JavaScript class?",
+        answers: [".", "#", "$"],
+        correctAnswer: "$",    
+  
+    },
+    {
+        question: "How many programming languages are there?",
+        answers: ["700", "1050", "120"],
+        correctAnswer: "700", 
+         
+    },
+
+]
