42e5a2ba3ebf945981181d18e07699078476d749
Zach Gutierrez <zg71603@gmail.com>
2024-01-05 00:10:36 -0800

    Code for timer

--- scipt.js
+++ scipt.js
@@ -45,3 +45,51 @@ var totalSeconds = 60;
 var holdPenaltyTime= 0;
 var penaltyTime = 15;
 
+ //this will start timer and track penalty times. Will call displayQuestion.
+quizStart.addEventListener("click", function(){
+   
+    //BUTTON HIDDEN!
+     this.style.visibility = "hidden";
+     //check if timer at zero
+     if (holdPenaltyTime === 0){
+         holdPenaltyTime = setInterval(function(){
+             totalSeconds--;
+             timerBox.textContent = "Time left: " + totalSeconds;
+     
+     if (totalSeconds === 0){
+         clearInterval(holdPenaltyTime);
+         timerBox.textContent = "Time is up!";
+ 
+         }
+     }, 1000);
+ }
+    
+ //console.log(quizStart, timerBox)
+ displayQuestion(questionAmount);
+ } 
+ 
+  ) 
+  //function that displays questions on screen. Need to call it in the function
+  //above with the eventlistener.
+  function displayQuestion(questionAmount){
+     questionBox.innerHTML = "";
+     createList.innerHTML = "";
+     //need a loop that goes through my question array. Setting variable for q and a
+         for (var i = 0; i <questionArray.length; i++){
+             var gameQuestion = questionArray[questionAmount].question;
+             var gameAnswers = questionArray[questionAmount].answers;
+             questionBox.textContent = gameQuestion;
+ 
+            // console.log(questionArray);
+         }
+         //bug fixed in function
+             gameAnswers.forEach(function (newQuestion){
+                 var listQuestion = document.createElement("li");
+                 listQuestion.textContent = newQuestion;
+                 questionBox.appendChild(createList);
+                 createList.appendChild(listQuestion);
+                 listQuestion.addEventListener("click", (evaluate));
+                // console.log(createList);
+          }
+      )
+ }
